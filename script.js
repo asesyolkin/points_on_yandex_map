24cbba881f0b4d33d74ed68670512931b1ca8687
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2019-03-26 16:21:51 +0300

    Исправлено перетаскивание точки между маршрутами, добавлена подсветка между пунктами во время перетаскивания

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 'use-strict';
-  
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+
 /*
-ymaps.ready(init);
-function init(){ 
-    // Создание карты.    
-    let myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-        // Порядок по умолчнию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [55.74, 37.63],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 10
-    });
-}
+  1. не меняется курсок на границе лишки, только внутри неё
+  2. переделать функцию deletePoint: делать проверку на класс, а не тег, удалять элемент через поиск по классу, а не через навигацию
 */
 
-let fieldCreateNewPoint = document.body.querySelector('.field-create-new-point');
-let listPointsRoutes = document.body.querySelector('.list-points-routes');
+let fieldCreateNewPoint = document.body.querySelector('.field-create-new-point'),
+listOfpoints = document.body.querySelector('.list-points-routes'),
+point,
+underPoint,
+previousPoint,
+ghostPoint,
+coordsOfPoint,
+coordsOfList,
+shiftY;
 
 fieldCreateNewPoint.addEventListener('keydown', addNewPoint);
-listPointsRoutes.addEventListener('click', deletePoint);
-listPointsRoutes.addEventListener('mousedown', movePoint);
+listOfpoints.addEventListener('click', deletePoint);
+listOfpoints.addEventListener('mousedown', movePoint);
 
 function addNewPoint(e) {
   if (e.keyCode !== 13 || this.value === '') return;
-
+  
   let newPoint = document.createElement('li');
   newPoint.classList.add('point');
   
@@ -41,7 +34,7 @@ function addNewPoint(e) {
     </span>  
   `;
   
-  listPointsRoutes.appendChild(newPoint);
+  listOfpoints.appendChild(newPoint);
   this.value = '';
 }
 
@@ -53,52 +46,119 @@ function deletePoint(e) {
 
 function movePoint(e) {
   if (e.target.closest('.delete-point') || !e.target.closest('.point')) return;
-
-  let point = e.target.closest('.point');
-  let listOfpoints = e.target.closest('.list-points-routes');
-  let coordsOfPoint = getCoords(point);
-  let coordsOfList = getCoords(listOfpoints);
-  let shiftY = e.pageY - coordsOfPoint.top;
-
+  
+  point = e.target.closest('.point');
+  ghostPoint = document.createElement('li'),
+  coordsOfPoint = getCoords(point),
+  coordsOfList = getCoords(listOfpoints),
+  shiftY = e.pageY - coordsOfPoint.top;
+  
   point.style.position = 'absolute';
   point.style.borderColor = 'rgb(0, 0, 0)';
+  point.style.pointerEvents = 'none';
+  point.style.cursor = 'grabbing';
   
-  let ghostPoint = document.createElement('li');
   ghostPoint.classList.add('ghostPoint');
+  ghostPoint.style.height = point.offsetHeight;
   listOfpoints.insertBefore(ghostPoint, point.nextElementSibling);
   moveAt(e);
   
   document.onmousemove = function(e) {
     moveAt(e);
+    backlightInsertions(e);
   };
   
   document.onmouseup = function(e) {
     document.onmousemove = null;
     
     point.style.cssText = '';
-    listOfpoints.removeChild(ghostPoint);
-    
     changePosition(e);
+    listOfpoints.removeChild(ghostPoint);
+    removeBacklightInsertions();
+    previousPoint = '';
     
     document.onmouseup = null;
   };
-  
-  function moveAt(e) {
-    const top = e.pageY - shiftY;
+}
 
-    if (top > coordsOfList.top - 10 && top < coordsOfList.bottom - 5) {
-      point.style.top = top + 'px';
-    };
-  }
+function moveAt(e) {
+  const top = e.pageY - shiftY;
   
-  function changePosition(e) {
-    let elem = document.elementFromPoint(e.pageX, e.pageY);
-    elem = elem.closest('.point');
-    console.log(elem);
-    listOfpoints.insertBefore(point, elem);
+  if (top > coordsOfList.top - 10 && top < coordsOfList.bottom - 5) {
+    point.style.top = top + 'px';
+  };
+}
+
+function changePosition(e) {
+  if (!previousPoint) return;
+
+  if (previousPoint.classList.contains('pointBefore')) {
+    listOfpoints.insertBefore(point, previousPoint);
+  } else {
+    listOfpoints.insertBefore(point, previousPoint.nextElementSibling);
   }
 }
+
+function backlightInsertions(e) {
+
+  underPoint = document.elementFromPoint(e.pageX, e.pageY).closest('.point');
   
+  if (!underPoint) {
+    underPoint = document.elementFromPoint(e.pageX, e.pageY).closest('.currentUnderPoint');
+  }
+  
+  if (!underPoint) return;
+  
+  if (previousPoint) {
+    if (previousPoint !== underPoint) {
+      removeBacklightInsertions();
+      previousPoint = underPoint;
+      underPointRemoveClass('point');
+      underPointAddClass('currentUnderPoint');
+    };
+  } else {
+    previousPoint = underPoint;
+    underPointRemoveClass('point');
+    underPointAddClass('currentUnderPoint');
+  };
+  
+  let coordsUnderPoint = underPoint.getBoundingClientRect();
+  let coordMiddleUnderPoint = (coordsUnderPoint.top + coordsUnderPoint.bottom) / 2;
+  
+  if (e.pageY > coordMiddleUnderPoint) {
+    if (underPointContainsClass('pointBefore')) {
+      underPointRemoveClass('pointBefore');
+    };
+    if (underPointContainsClass('pointAfter')) return;
+    else underPointAddClass('pointAfter');
+  } else {
+    if (underPointContainsClass('pointAfter')) {
+      underPointRemoveClass('pointAfter');
+    };
+    if (underPointContainsClass('pointBefore')) return;
+    else underPointAddClass('pointBefore');
+  }
+  
+  function underPointAddClass(nameClass) {
+    underPoint.classList.add(nameClass);
+  }
+  
+  function underPointRemoveClass(nameClass) {
+    underPoint.classList.remove(nameClass);
+  }
+  
+  function underPointContainsClass(nameClass) {
+    return underPoint.classList.contains(nameClass);
+  }
+}
+
+function removeBacklightInsertions() {
+  if (!previousPoint) return;
+
+  previousPoint.classList.remove('currentUnderPoint', 'pointBefore', 'pointAfter');
+  previousPoint.classList.add('point');
+}
+
 function getCoords(elem) {
   let box = elem.getBoundingClientRect();
   return {
@@ -111,3 +171,22 @@ function getCoords(elem) {
 
 // console.log(getCoords(document.querySelector('.point')));
 // console.log(getCoords(document.querySelector('.list-points-routes')));
+
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+
+// ymaps.ready(init);
+// function init(){ 
+//   // Создание карты.    
+//   let myMap = new ymaps.Map("map", {
+//       // Координаты центра карты.
+//       // Порядок по умолчнию: «широта, долгота».
+//       // Чтобы не определять координаты центра карты вручную,
+//       // воспользуйтесь инструментом Определение координат.
+//       center: [55.74, 37.63],
+//       // Уровень масштабирования. Допустимые значения:
+//       // от 0 (весь мир) до 19.
+//       zoom: 10
+//   });
+// }
